9511073d2c1a4eb01c5ef96680fdd2eb8f728dcf
roberte <robertonsate23@gmail.com>
2026-06-11 19:49:31 +0300

    feat: add order unit tests

---
+++ tests/test_orders.py
@@ -0,0 +1,25 @@
+import unittest
+
+from utils.validators import validate_quantity
+from utils.helpers import calculate_change
+
+
+class TestOrders(unittest.TestCase):
+
+    def test_valid_quantity(self):
+        self.assertTrue(validate_quantity(3))
+
+
+    def test_invalid_quantity(self):
+        self.assertFalse(validate_quantity(0))
+
+
+    def test_change_calculation(self):
+        change = calculate_change(500, 300)
+
+        self.assertEqual(change, 200)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
